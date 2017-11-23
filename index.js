bf3d3b14fb994a0adb132687ea3c340be5ba3e38
霍文轩 <1078954008@qq.com>
2017-11-24 00:02:35 +0800

    优化
    使用async代替async/await，使用低版本node也可以运行，并且对不同的场景进行并行或串行的网络请求。使用superagent代替http和iconv进行网络请求和网页获取。增加超时功能。增加输出超时、错误的页面和详情的功能。

--- index.js
+++ index.js
@@ -1,90 +1,131 @@
-﻿
-var cheerio = require('cheerio');
-var http = require('http');
-var iconv = require('iconv-lite');
-var request = require('request');
-var fs = require('fs');
+﻿var cheerio = require('cheerio');
+var async = require('async');
+var superagent = require('superagent')
+require('superagent-charset')(superagent)
 
-let Host = 'http://www.79kmkm.com'
+/*
+ * TODO: 封装timeout方法
+ * TODO: 通过多页的url数组批量获取视频地址
+ * TODO: 通过多个视频详情的url数组批量获取视频地址
+ */
 
-function getDetail(url) {
-  if (!url) {
-    url = Host + ':8888/video/2017-11/29145.html'
-  }
-  return new Promise((resolve)=>{
-    http.get(url, function (sres) {
-      var chunks = [];
-      sres.on('data', function (chunk) {
-        chunks.push(chunk);
-      });
-      sres.on('end', function () {
-        var html = iconv.decode(Buffer.concat(chunks), 'gb2312');
-        var $ = cheerio.load(html, {decodeEntities: false});
-        // 获取到的数据只有script中有mp4
-        let title = $('title').html().replace('在线观看-国产-27pao视频免费在线视频', '')
-        // console.log(title)
-        $("script").each(function (idx, element) {
-          let script = $(element).html()
-          if (script.indexOf('flashvars') !== -1) {
-            let start = script.indexOf("f:'") + 3
-            let end = script.indexOf(".mp4") + 4
-            let mp4 = script.substring(start, end)
-            console.log(mp4);
-            resolve(mp4)
-          }
+// http://www.68kvkv.com/ http://www.91kmkm.com/ http://www.25kmkm.com/ http://www.21kmkm.com/ http://www.92kmkm.com/
+// http://www.79kmkm.com/ 67popo.com 85kvkv.com
+let Host = 'http://www.85kvkv.com'
+let timeoutSecond = 2
+
+let detailTimeoutList = []
+let detailErrorList = []
+let pageTimeoutList = []
+let pageErrorList = []
+
+function logError(text, url, error) {
+    console.log(text + url)
+    console.log('错误原因：' + error + '\n')
+}
+
+// eg: /video/2017-11/29494.html
+function getDetail(url, next) {
+    let timer = setTimeout(()=>{
+        // console.log('获取详情超时：'+ url)
+        detailTimeoutList.push(url)
+        next && next()
+        next = null
+        clearTimeout(timer)
+    }, timeoutSecond * 1000)
+
+    superagent.get(url).charset('gb2312').end(function (err, res) {
+        clearTimeout(timer)
+
+        if (err) {
+            // logError('获取详情错误：', url, err)
+            detailErrorList.push(url)
+            next && next()
+        } else {
+            let $ = cheerio.load(res.text, {decodeEntities: false});
+            // 获取到的数据只有script中有mp4
+            let title = $('title').html().replace('在线观看-国产-27pao视频免费在线视频', '')
+            $("script").each(function (idx, element) {
+                let script = $(element).html()
+                if (script.indexOf('flashvars') !== -1) {
+                    let start = script.indexOf("f:'") + 3
+                    let end = script.indexOf(".mp4") + 4
+                    let mp4 = script.substring(start, end)
+                    next && console.log(mp4);
+                }
+            })
+            next && next()
+        }
+    })
+}
+
+// getDetail('http://www.85kvkv.com/video/2017-11/29547.html')
+
+// getDetail(Host + '/video/2017-11/29494.html')
+
+// eg: /diao/se57.html  /diao/se57_2.html
+function getList(url, next, limit, count) {
+    let timer = setTimeout(()=>{
+        pageTimeoutList.push(url)
+        // console.log('获取一页超时'+ url)
+        next && next()
+        next = null
+        clearTimeout(timer)
+    }, timeoutSecond * 1000)
+
+    superagent.get(url).charset('gb2312').end(function (err, res) {
+        clearTimeout(timer)
+
+        if (err) {
+            // logError('获取一页错误：', url, err)
+            pageTimeoutList.push(url)
+            next && next()
+        } else {
+            if (limit !== undefined && limit !== null && count !== undefined && count !== null) {
+                console.log(`正在获取第${limit}页，共${count}页`)
+            }
+
+            let $ = cheerio.load(res.text, {decodeEntities: false});
+            let asyncList = []
+            $('.video_box a').each(function (index, element) {
+                let $element = $(element);
+                let detailUrl = Host + $element.attr('href')
+                asyncList.push((next2) => getDetail(detailUrl, next2))
+            });
+            // 并行
+            async.parallel(asyncList, (err, res) => {
+                next && next()
+            })
+        }
+    })
+}
+
+// getList(Host + '/diao/se57.html')
+
+function getLists(start, end) {
+    let asyncList = []
+    for (let i = start; i < end; i++) {
+        let url = Host + '/diao/se57.html'
+        if (i !== 0) url = Host + '/diao/se57_' + (i + 1) + '.html'
+
+        asyncList.push((next) => {
+            // 不知为何，直接传next不可以，必须在这里执行
+            getList(url, ()=>{next()}, i, end - start)
         })
-      });
-    });
-  })
-
-}
-
-async function getList(url) {
-  if (!url) {
-    url = Host + ':8888/diao/se57.html'
-  }
-  let url2 = Host + ':8888/diao/se57_2.html'
-
-  return new Promise((resolve)=>{
-    http.get(url, function (sres) {
-      var chunks = [];
-      sres.on('data', function (chunk) {
-        chunks.push(chunk);
-      });
-      sres.on('end', function () {
-        var html = iconv.decode(Buffer.concat(chunks), 'gb2312');
-        var $ = cheerio.load(html, {decodeEntities: false});
-        $('.video_box a').each(async function (index, element) {
-          var $element = $(element);
-          let detailUrl = "http://www.63kmkm.com:8888" + $element.attr('href')
-          await getDetail(detailUrl)
-        });
-        resolve()
-      });
-    });
-  })
-
-
-}
-
-async function getLists(start, end) {
-  for(let i = start; i < end; i++) {
-    if (i === 0) {
-      await getList(Host + ':8888/diao/se57.html')
-    } else {
-      await getList(Host + ':8888/diao/se57_' + (i+1) + '.html')
     }
-    console.log('=========', i, '/', end - start)
-    
-  }
+
+    // 串行
+    async.series(asyncList, (err, res) => {
+        console.log('详情超时：')
+        console.log(detailTimeoutList)
+        console.log('详情错误：')
+        console.log(detailErrorList)
+        console.log('一页超时：')
+        console.log(pageTimeoutList)
+        console.log('一页错误：')
+        console.log(pageErrorList)
+    })
 }
 
 getLists(0, 15)
-// getList('http://www.79kmkm.com' + ':8888/diao/se57.html').then(()=>{}).catch(()=>{})
-// getList('http://www.21kmkm.com' + ':8888/diao/se57.html').then(()=>{}).catch(()=>{})
-// getList('http://www.92kmkm.com' + ':8888/diao/se57.html').then(()=>{}).catch(()=>{})
-// getList('http://www.25kmkm.com' + ':8888/diao/se57.html').then(()=>{}).catch(()=>{})
-
-// getList('http://www.91kmkm.com' + ':8888/diao/se57.html').then(()=>{}).catch(()=>{})
-// getList('http://www.68kmkm.com' + ':8888/diao/se57.html').then(()=>{}).catch(()=>{})
 
