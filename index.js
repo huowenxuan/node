ba0f616b0842786997e7d2626d9393c40d640f4b
霍微信 <wahid@girtu.com>
2018-05-31 10:21:42 +0800

    update index

---
+++ index.js
@@ -0,0 +1,15 @@
+let one = require('./crawler/1.js')
+let two = require('./crawler/2.js')
+let three = require('./crawler/3.js')
+let nine = require('./crawler/9.js')
+let dytt = require('./crawler/dytt.js')
+let p = require('./crawler/p.js')
+
+module.exports = {
+  dytt,
+  p,
+  onem
+  two,
+  three,
+  nine,
+}
