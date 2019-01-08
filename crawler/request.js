7c8ee13e8241b82bb8a2de63211f21e19c4ad79b
Wahid <wahid@girtu.com>
2019-01-08 10:10:46 +0800

    update

--- crawler/request.js
+++ crawler/request.js
@@ -8,11 +8,11 @@ function getHTML(url, options) {
   options = options || {}
   let charset = options.charset || 'gb2312'
 
-  return new Promise((resolve, reject)=>{
+  return new Promise((resolve, reject) => {
     superagent.get(url)
       .set({
-        Referer: url,
-        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
+        'Accept': 'text/html',
+        'Content-Type': 'text/html',
       })
       .timeout(10000)
       .charset(charset)
@@ -28,7 +28,7 @@ function getHTML(url, options) {
   })
 }
 
-// 不支持charset，但是可以上去9
+// 不支持charset，但是支持ionic
 function getHTML9(url, options) {
   return axios({
     data: null,
@@ -36,10 +36,12 @@ function getHTML9(url, options) {
     url,
     timeout: 10000,
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
+      'Accept': 'text/html',
+      'Content-Type': 'text/html',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+      Cookie: '__cfduid=d09699ed4759eab82379833ee2bd3c5841534496967; __utma=50351329.1296256659.1534496968.1534496968.1534496968.1; __utmb=50351329.0.10.1534496968; __utmc=50351329; __utmz=50351329.1534496968.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); CLIPSHARE=88krugfvudbtulvmacl6o6s7f7; __51cke__=; __dtsu=D9E9B66BCE90765BD107181D02F9B7C7; watch_times=1; __tins__3878067=%7B%22sid%22%3A%201534496972740%2C%20%22vd%22%3A%202%2C%20%22expires%22%3A%201534498819895%7D; __51laig__=2'
     }
-  }).then((res) => res.data)
+  }).then((res) => cheerio.load(res.data, {decodeEntities: false}))
 }
 
 module.exports = {
